612107ebeed1edfcae2fd8f98f6725f8b241955e
franciscobuchini <franciscobuchini@gmail.com>
2024-03-18 23:25:33 +1300

    bento grid y bento component, colours and borders

--- src/Bento.jsx
+++ src/Bento.jsx
@@ -1,8 +1,12 @@
-export default function Bento() {
+export default function Bento({ size, model, h2, p }) {
 
   return (
-    <div className='Bento'>
-
+    <div className={`Bento ${size} ${model}`}>
+      <div className="Text">
+        <h2>{h2}</h2>
+        <p>{p}</p>
+      </div>
+      <img className="Image" src='#'/>
     </div>
     )
   }
--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -4,15 +4,14 @@ export default function BentoGrid() {
 
   return (
     <div className='BentoGrid'>
-      <Bento/>
-      <Bento/>
-      <Bento/>
-      <Bento/>
-      <Bento/>
-      <Bento/>
-      <Bento/>
-      <Bento/>
-      <Bento/>
+      <Bento size='B5x3' model='VT1I2' h2='Título del Bento' p='Párrafo dentro del Bento'/>
+      <Bento size='B3x6' model='HT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
+      <Bento size='B6x3' model='VT1I2' h2='Título del Bento' p='Párrafo dentro del Bento'/>
+      <Bento size='B5x6' model='HT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
+      <Bento size='B3x3' model='VT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
+      <Bento size='B5x3' model='VT1I2' h2='Título del Bento' p='Párrafo dentro del Bento'/>
+      <Bento size='B3x6' model='HT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
+      <Bento size='B3x3' model='VT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
     </div>
     )
   }
