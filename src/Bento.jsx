7a10ef1e4e59862d96ac99d277b11b7fbd83688f
franciscobuchini <franciscobuchini@gmail.com>
2024-03-19 11:19:38 +1300

    created the BentoImage and CSS propieties, tried example image, also created a new branch cafe-cafe

--- src/Bento.jsx
+++ src/Bento.jsx
@@ -1,12 +1,12 @@
-export default function Bento({ size, model, h2, p, bg }) {
-
+export default function Bento({ size, model, h2, p, bg, img }) {
   return (
     <div className={`Bento ${size} ${model}`} style={{ backgroundColor: bg }}>
+      <img className="Image" src={img}/>
       <div className="Text">
         <h2>{h2}</h2>
         <p>{p}</p>
       </div>
-      <img className="Image" src='#'/>
+      
     </div>
     )
   }
--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -1,4 +1,5 @@
 import Bento from './Bento'
+import CafeCafe from './Assets/CafeCafe.svg'
 
 export default function BentoGrid() {
 
@@ -7,7 +8,7 @@ export default function BentoGrid() {
       <Bento size='B5x3' model='VT1I2' h2='Título del Bento' p='Párrafo dentro del Bento' bg='white'/>
       <Bento size='B3x6' model='HT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
       <Bento size='B6x3' model='VT1I2' h2='Título del Bento' p='Párrafo dentro del Bento'/>
-      <Bento size='B5x6' model='HT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
+      <Bento size='B5x6' model='BentoImage' h2='Título del Bento' p='Párrafo dentro del Bento' img={CafeCafe}/>
       <Bento size='B3x3' model='VT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
       <Bento size='B5x3' model='VT1I2' h2='Título del Bento' p='Párrafo dentro del Bento'/>
       <Bento size='B3x6' model='HT1I1' h2='Título del Bento' p='Párrafo dentro del Bento'/>
