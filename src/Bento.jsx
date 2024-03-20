65e081740d6616771160bab9ca0e1160ef0ad283
franciscobuchini <franciscobuchini@gmail.com>
2024-03-20 16:22:34 +1300

    cafe-cafe fixed, hover and text align

--- src/Bento.jsx
+++ src/Bento.jsx
@@ -1,6 +1,6 @@
-export default function Bento({ size, model, h2, p, bg, img }) {
+export default function Bento({ size, model, h2, p, bg, img, tpos }) {
   return (
-    <div className={`Bento Cen ${size} ${model}`} style={{ backgroundColor: bg }}>
+    <div className={`Bento ${tpos} ${size} ${model}`} style={{ backgroundColor: bg }}>
       <img className="Image" src={img}/>
       <div className="Text">
         <h2>{h2}</h2>
--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -1,27 +1,84 @@
 import Bento from './Bento'
 import CafeCafe01 from './Assets/CafeCafe01.svg'
 import CafeCafe02 from './Assets/CafeCafe02.svg'
-import CafeCafe04 from './Assets/CafeCafe04.svg'
 import CafeCafe05 from './Assets/CafeCafe05.svg'
 import CafeCafe06 from './Assets/CafeCafe06.svg'
 import CafeCafe07 from './Assets/CafeCafe07.svg'
 import CafeCafe08 from './Assets/CafeCafe08.svg'
 import CafeCafe09 from './Assets/CafeCafe09.svg'
+import CafeCafe10 from './Assets/CafeCafe10.svg'
+import CafeGrain from './Assets/CafeGrain.png'
 import Creator from './Assets/Creator.png'
 
 export default function BentoGrid() {
 
   return (
     <div className='BentoGrid'>
-      <Bento size='B3x6' model='BentoImage' img={CafeCafe08}/>
-      <Bento size='B5x6' model='BentoImage' img={CafeCafe01}/>
-      <Bento size='B6x3' model='BentoImage' img={CafeCafe09}/>
-      <Bento size='B3x3' model='BentoImage' img={CafeCafe02}/>
-      <Bento size='B3x6' model='BentoImage' img={CafeCafe06}/>
-      <Bento size='B3x3' model='BentoImage' img={CafeCafe04}/>
-      <Bento size='B3x6' model='BentoImage' img={CafeCafe07}/>
-      <Bento size='B3x6' model='BentoImage' img={CafeCafe05}/>
-      <Bento size='B1x6' model='HT2I1' h2='Café Café Coffeeshop' p='by Pedro Miguel Xarepe' img={Creator}/>
+      <Bento
+      size='B3x6'
+      model='BImg'
+      h2='Meals'
+      img={CafeCafe07}/>
+
+      <Bento
+      size='B5x6'
+      model='BImg'
+      h2='Café Café Coffeeshop'
+      p='Web Design by Francisco Buchini'
+      img={CafeCafe01}
+      tpos='TextTop'/>
+
+      <Bento
+      size='B6x3'
+      model='BImg'
+      img={CafeCafe09}/>
+
+      <Bento
+      size='B3x3'
+      model='BImg' 
+      img={CafeCafe02}/>
+
+      <Bento
+      size='B3x6'
+      model='BImg'
+      h2='Fast Food'
+      img={CafeCafe06}/>
+
+      <Bento
+      size='B3x3'
+      model='VT1I2'
+      h2='Coffee Magic!'
+      p='Join us for an unforgettable coffee journey today!'
+      img={CafeGrain}
+      tpos='TextCen'/>
+
+      <Bento
+      size='B3x6'
+      model='BImg' 
+      h2='Hot Drinks'
+      img={CafeCafe08}
+      tpos='TextTop'/>
+
+      <Bento
+      size='B3x6'
+      model='BImg' 
+      h2='Cold Drinks'
+      img={CafeCafe05}
+      tpos='TextTop'/>
+
+      <Bento
+      size='B1x6'
+      model='HT2I1'
+      h2='Café Café Coffeeshop'
+      p='Branding Design by Pedro Miguel Xarepe'
+      img={Creator}
+      tpos='TextCen'/>
+
+      <Bento
+      size='B3x12'
+      model='BImg'
+      img={CafeCafe10}/>
+
     </div>
     )
   }
