a922df5bb895793032ea28f5fc676d469f533887
franciscobuchini <franciscobuchini@gmail.com>
2024-04-11 14:31:06 +0800

    deleted the styles class and trying webp images

--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -8,7 +8,7 @@ export default function BentoGrid() {
   return (
     <main className='BentoGrid'>
       <Bento sv='3' sh='2' />
-      <Bento sv='2' sh='4' h='Proximas tareas:' p='Necesito crear una armonía entre .Imagen, .Text y el nuevo .Component. Estaba pensando que .Imagen tiene que ir con un "position: absolute" y que ocupe el 100% del .Bento. Que la imagen ocupe los espacios desde la creacion de la misma y entonces solo me queda organizar los elementos .Text y .Component.' bgc='taxbreak' hcl='gold' pcl='white' ta='center'/>
+      <Bento sv='2' sh='4' h='Proximas tareas:' p='Necesito crear una armonía entre .Imagen, .Text y el nuevo .Component. Estaba pensando que .Imagen tiene que ir con un "position: absolute" y que ocupe el 100% del .Bento. Que la imagen ocupe los espacios desde la creacion de la misma y entonces solo me queda organizar los elementos .Text y .Component.' bgc='taxbreak' hcl='gold' pcl='white' ta='center' tp='center'/>
       <Bento sv='4' sh='2' />
       <Bento sv='3' sh='4' st='IMG' img={CafeCafe01}/>
       <Bento st='VTI' sv='2' sh='2' com={Switch} h='Switch example' tp='center' ta='center' bgc='christine' fd='column'/>
