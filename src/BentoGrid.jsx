51368a2a60db5964686701be2397d8270d137104
franciscobuchini <franciscobuchini@gmail.com>
2024-04-21 17:07:41 -0700

    deleted some useless lines and components

--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -1,24 +1,19 @@
 import Bento from './Bento'
-import Switch from './Components/Switch'
-import ThemeSwitch from './Components/ThemeSwitch'
 import Button from './Components/Button'
-import Img01 from './Assets/Image01.webp'
-import Img02 from './Assets/Image02.webp'
-import Img03 from './Assets/Image03.webp'
 
 export default function BentoGrid() {
 
   return (
     <main className='BentoGrid'>
-      <Bento Bsv='2' Bsh='4' Bbg='taxbreak' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Tpc='white' Tta='center' Tjc='center' />
-      <Bento Bsv='2' Bsh='4' Bst='B1' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Com={Button} />
-      <Bento Bsv='2' Bsh='4' Bst='B2' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Com={Button} />
-      <Bento Bsv='2' Bsh='4' Bst='B3' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Com={Button} />
-      <Bento Bsv='2' Bsh='4' Bst='B4' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Com={Button} />
-      <Bento Bsv='2' Bsh='4' Bst='B5' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Com={Button} />
-      <Bento Bsv='2' Bsh='4' Bst='B6' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Com={Button} />
-      <Bento Bsv='2' Bsh='4' Bst='B7' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Com={Button} />
-      <Bento Bsv='2' Bsh='4' Bst='B8' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI. 5)Revisar y rehacer BFD, BJC, TJC, TTA, CJC.' Com={Button} />
+      <Bento Bsv='2' Bsh='4' Bbg='taxbreak' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Tpc='white' Tta='center' Tjc='center' />
+      <Bento Bsv='2' Bsh='4' Bst='B1' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Com={Button} />
+      <Bento Bsv='2' Bsh='4' Bst='B2' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Com={Button} />
+      <Bento Bsv='2' Bsh='4' Bst='B3' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Com={Button} />
+      <Bento Bsv='2' Bsh='4' Bst='B4' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Com={Button} />
+      <Bento Bsv='2' Bsh='4' Bst='B5' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Com={Button} />
+      <Bento Bsv='2' Bsh='4' Bst='B6' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Com={Button} />
+      <Bento Bsv='2' Bsh='4' Bst='B7' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Com={Button} />
+      <Bento Bsv='2' Bsh='4' Bst='B8' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) Bentos de 8 columnas. 4) Logo Bento UI.' Com={Button} />
     </main>
     )
   }
