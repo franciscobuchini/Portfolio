77f2bcb3f48e36690243bcdd3c7c6a87b9937109
franciscobuchini <franciscobuchini@gmail.com>
2024-04-25 13:17:58 -0700

    mobilefirst testing

--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -7,7 +7,10 @@ export default function BentoGrid() {
   return (
     <main className='BentoGrid'>
       <Bento Bsv='2' Bsh='4' Bst='B9' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) problema con la pantalla estirada en la version mobile. 4) Agregar APIs.' Tta='center' Tjc='center' />
-      <Bento Bsv='2' Bsh='2' Bst='B7' Com={Button} />
+      <Bento Bsv='4' Bsh='2' Bst='B7' Com={Button} />
+      <Bento Bsv='2' Bsh='2' Bst='B7' Com={ThemeSwitch} />
+      <Bento Bsv='2' Bsh='4' Bst='B9' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) problema con la pantalla estirada en la version mobile. 4) Agregar APIs.' Tta='center' Tjc='center' />
+      <Bento Bsv='4' Bsh='2' Bst='B7' Com={Button} />
       <Bento Bsv='2' Bsh='2' Bst='B7' Com={ThemeSwitch} />
     </main>
     )
