42a5dec34a39cb4c37b43a4af2134bd9d33d91da
franciscobuchini <franciscobuchini@gmail.com>
2024-04-26 15:24:20 -0700

    added the first API: Here Map

--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -5,13 +5,9 @@ import ThemeSwitch from './Components/ThemeSwitch'
 import Button from './Components/Button'
 
 export default function BentoGrid() {
-  useEffect(() => {
-    // Aquí puedes ejecutar código después de que el DOM esté completamente cargado
-  }, []) // Utilizamos un array vacío de dependencias para asegurar que el efecto se ejecute solo una vez
-
   return (
     <main className='BentoGrid'>
-      <Bento Bsv='2' Bsh='4' Bst='B9' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax. 3) problema con la pantalla estirada en la version mobile. 4) Agregar APIs.' />
+      <Bento Bsv='2' Bsh='4' Bst='B9' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax.' />
       <Map Bsv='4' Bsh='2' />
     </main>
   )
