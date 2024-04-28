af3a2aedf38ebdb9c407f16c022c0e1580d0f49d
franciscobuchini <franciscobuchini@gmail.com>
2024-04-28 14:59:55 -0700

    edited color and theme on the map

--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -4,13 +4,15 @@ import ProgressBar from './Components/ProgressBar'
 import ThemeSwitch from './Components/ThemeSwitch'
 import Button from './Components/Button'
 import ReactBG from './Assets/ReactBG.webp'
+import FigmaBG from './Assets/FigmaBG.webp'
 
 export default function BentoGrid() {
   return (
     <main className='BentoGrid'>
-      <Bento Bsv='2' Bsh='4' Bst='B1' Th='Proximas tareas:' Tp='1) Download Button. 2) Mini-logos en progressbar. 3) Texto en mapa.' />
+      <Bento Bsv='2' Bsh='4' Bst='B1' Th='Proximas tareas:' Tp='1) Download Button. 2) Mini-logos en progressbar. 3) Theme del mapa.' />
       <Map Bsv='4' Bsh='2' Th='My location:'/>
-      <Bento Bsv='2' Bsh='2' Bst='B7' IBimg={ReactBG} PBn='4' PBd='10' PBh='React:' Com={ProgressBar}/>
+      <Bento Bsv='1' Bsh='2' Bst='B7' IBimg={ReactBG} PBn='4' PBd='10' PBh='React:' Com={ProgressBar}/>
+      <Bento Bsv='1' Bsh='2' Bst='B7' IBimg={FigmaBG} PBn='4' PBd='10' PBh='Figma:' Com={ProgressBar}/>
       <Bento Bsv='2' Bsh='2' Bst='B7' Th='Texto de prueba' Com={Button}/>
       <Bento Bsv='2' Bsh='2' Bst='B7' Th='Theme switcher:' Com={ThemeSwitch}/>
     </main>
