7335099f10d6b554c08182d8488151cd43224e07
franciscobuchini <franciscobuchini@gmail.com>
2024-04-28 12:49:04 -0700

    edited progressbar and colors of the themes. Now fixing text color problem

--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -3,13 +3,14 @@ import Map from './Components/Map'
 import ProgressBar from './Components/ProgressBar'
 import ThemeSwitch from './Components/ThemeSwitch'
 import Button from './Components/Button'
+import ReactBG from './Assets/ReactBG.webp'
 
 export default function BentoGrid() {
   return (
     <main className='BentoGrid'>
-      <Bento Bsv='2' Bsh='4' Bst='B7' Th='Proximas tareas:' Tp='1) Animación y mods para botones. 2) Investigar patrones y probar backgrounds parallax.' />
+      <Bento Bsv='2' Bsh='4' Bst='B7' Th='Proximas tareas:' Tp='1) Download Button. 2) Mini-logos en progressbar. 3) Texto en mapa.' />
       <Map Bsv='4' Bsh='2' />
-      <Bento Bsv='2' Bsh='2' Bst='B7' Th='Experience' PBn='2' PBd='10' PBh='React JS:' Com={ProgressBar}/>
+      <Bento Bsv='2' Bsh='2' Bst='B7' IBimg={ReactBG} PBn='2' PBd='10' PBh='React JS:' Com={ProgressBar}/>
       <Bento Bsv='2' Bsh='2' Bst='B7' Th='Texto de prueba' Com={Button}/>
       <Bento Bsv='2' Bsh='2' Bst='B7' Th='Theme switcher:' Com={ThemeSwitch}/>
     </main>
