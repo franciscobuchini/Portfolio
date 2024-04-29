392ffdf97cf313f99df1c9f3a0a35dde9fd52314
franciscobuchini <franciscobuchini@gmail.com>
2024-04-28 17:58:50 -0700

    modifying code for the map

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -3,15 +3,15 @@ import React, { useEffect } from 'react'
 export default function Map({ Bsv, Bsh, Th }) {
   useEffect(() => {
     // Aquí puedes ejecutar código específico para inicializar el mapa después de que el DOM esté completamente cargado
-    var platform = new H.service.Platform({ 'apikey': 'd7uc0gBf8RxxYH4FpQQ8nzNhvxqcTAJYrt77_2RS6OQ' })
+    let platform = new H.service.Platform({ 'apikey': 'd7uc0gBf8RxxYH4FpQQ8nzNhvxqcTAJYrt77_2RS6OQ' })
     
-    var defaultLayers = platform.createDefaultLayers()
+    let defaultLayers = platform.createDefaultLayers()
 
-    var map = new H.Map(
+    let map = new H.Map(
       document.getElementById('MapContainer'),
       defaultLayers.vector.normal.map,
       {
-        zoom: 10,
+        zoom: 12,
         center: { lat: 39.244233, lng: -119.950676 }
       }
     )
@@ -19,12 +19,12 @@ export default function Map({ Bsv, Bsh, Th }) {
   }, []) // Utilizamos un array vacío de dependencias para asegurar que el efecto se ejecute solo una vez
 
   return (
-    <section className="MapSection" style={{
+    <section className="Map" style={{
       gridRow: `span ${Bsv}`,
       gridColumn: `span ${Bsh}`,
       aspectRatio: `${Bsh}/${Bsv}`,
     }}>
-      <div id="MapContainer" className="Map"> </div>
+      <div id="MapContainer" className="MapDiv"> </div>
       <div className="Text"> <h2>{Th}</h2> </div>
     </section>
   )
