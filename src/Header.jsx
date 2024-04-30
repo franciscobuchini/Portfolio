6e79a1f24dd0d4fb6548318f87a632de3d84047b
franciscobuchini <franciscobuchini@gmail.com>
2024-04-29 21:57:52 -0700

    added svg with theme changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,14 @@
-import Logo from './Assets/Logo.webp'
+import Logo from './Assets/Logo.svg'
 export default function Header () {
   return (
     <header>
-      <img src={Logo} alt="BentoBox UI Logo"/>
+      <svg  viewBox="0 0 600 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="20" y="20" width="560" height="560" rx="80"  stroke-width="40"/>
+        <path d="M100 100H187.5C235.825 100 275 139.175 275 187.5V187.5C275 235.825 235.825 275 187.5 275H100V100Z" fill="#1E1E1E"/>
+        <path d="M500 100L500 187.5C500 235.825 460.825 275 412.5 275V275C364.175 275 325 235.825 325 187.5L325 100L500 100Z" fill="#1E1E1E"/>
+        <path d="M500 324L500 499V499C472.386 499 450 476.614 450 449L450 374C450 346.386 472.386 324 500 324V324Z" fill="#1E1E1E"/>
+        <path d="M100 325H312.5C360.825 325 400 364.175 400 412.5V412.5C400 460.825 360.825 500 312.5 500H100V325Z" fill="#1E1E1E"/>
+      </svg>
       <nav className='Nav'>
         <ul>
           <li><a>-</a></li>
