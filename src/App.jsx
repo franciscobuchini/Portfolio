6e9bb7d495d2d1d9281a5bead8cddc816036489c
franciscobuchini <franciscobuchini@gmail.com>
2024-04-30 14:02:04 -0700

    moved everything from bentogrid to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import BentoGrid from "./BentoGrid"
 import Header from "./Header"
 import Footer from "./Footer"
+import React, { useState } from 'react';
+import Bento from './Bento';
+import Map from './Components/Map';
+import ThemeSwitch from './Components/ThemeSwitch';
+import ProgressBar from './Components/ProgressBar';
+import ReactImg from './Assets/ReactImg.webp';
+import Button from './Components/Button'
+import Download from './Components/Download';
 
 function App() {
+
+  //Map Theme switcher
+  const [theme, setTheme] = useState("light");
+  const handleSwitch = (isChecked) => {setTheme(isChecked ? "dark" : "light");}
   
   return (
     <>
       <Header/>
-      <BentoGrid/>
+      <BentoGrid>
+        <Bento Bsv='2' Bsh='4' Bst='B1' Th='Proximas tareas:' Tp='1) Crear labels.' />
+        <Map Bsv='4' Bsh='2' Th='My location:' ARc='Y' theme={theme} />
+        <Bento Bsv='1' Bsh='2' Bst='B7' PBn='4' PBd='10' PBt='React:' PBimg={ReactImg} Com={ProgressBar}/>
+        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Theme switcher:' Com={() => <ThemeSwitch onChange={handleSwitch} />} />
+        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Button example:' Com={Button} />
+        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Download example:' Com={Download} />
+      </BentoGrid>
       <Footer/>
     </>
   )
--- src/BentoGrid.jsx
+++ src/BentoGrid.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from 'react';
-import Bento from './Bento';
-import Map from './Components/Map';
-import ThemeSwitch from './Components/ThemeSwitch';
-import ProgressBar from './Components/ProgressBar';
-import ReactImg from './Assets/ReactImg.webp';
-import Button from './Components/Button'
-import Download from './Components/Download';
-
-export default function BentoGrid() {
-  
-  //Map Theme switcher
-  const [theme, setTheme] = useState("light");
-  const handleSwitch = (isChecked) => {setTheme(isChecked ? "dark" : "light");}
-
+export default function BentoGrid(props) {
   return (
     <main className='BentoGrid'>
-      <Bento Bsv='2' Bsh='4' Bst='B1' Th='Proximas tareas:' Tp='1) Crear labels.' />
-      <Map Bsv='4' Bsh='2' Th='My location:' ARc='Y' theme={theme} />
-      <Bento Bsv='1' Bsh='2' Bst='B7' PBn='4' PBd='10' PBt='React:' PBimg={ReactImg} Com={ProgressBar}/>
-      <Bento Bsv='2' Bsh='2' Bst='B7' Th='Theme switcher:' Com={() => <ThemeSwitch onChange={handleSwitch} />} />
-      <Bento Bsv='2' Bsh='2' Bst='B7' Th='Button example:' Com={Button} />
-      <Bento Bsv='2' Bsh='2' Bst='B7' Th='Download example:' Com={Download} />
+      {props.children}
     </main>
   )
 }
