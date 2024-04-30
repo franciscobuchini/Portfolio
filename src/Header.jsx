da7e1b3c5613bc9880fa8c43891d78d0048bd3b3
franciscobuchini <franciscobuchini@gmail.com>
2024-04-30 11:05:23 -0700

    fixing some design problems

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ export default function Header () {
   return (
     <header>
       <svg  viewBox="0 0 600 600" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect x="20" y="20" width="560" height="560" rx="80"  stroke-width="40"/>
+        <rect x="20" y="20" width="560" height="560" rx="80"/>
         <path d="M100 100H187.5C235.825 100 275 139.175 275 187.5V187.5C275 235.825 235.825 275 187.5 275H100V100Z" fill="#1E1E1E"/>
         <path d="M500 100L500 187.5C500 235.825 460.825 275 412.5 275V275C364.175 275 325 235.825 325 187.5L325 100L500 100Z" fill="#1E1E1E"/>
         <path d="M500 324L500 499V499C472.386 499 450 476.614 450 449L450 374C450 346.386 472.386 324 500 324V324Z" fill="#1E1E1E"/>
