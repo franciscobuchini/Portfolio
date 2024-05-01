38bd194042628f9f6042c0f3c3f3dcf2e091581d
franciscobuchini <franciscobuchini@gmail.com>
2024-04-30 19:26:18 -0700

    modified the separator component responsive

--- src/App.jsx
+++ src/App.jsx
@@ -28,22 +28,6 @@ function App() {
         <Bento Bsv='2' Bsh='2' Bst='B7' Th='Button example:' Com={Button} />
         <Bento Bsv='2' Bsh='2' Bst='B7' Th='Download example:' Com={Download} />
       </BentoGrid>
-      <BentoGrid Sh='HOLAAA' Sp='Chauuuuuñalsdañsdkañsldkañs ldkasñdaskdñ akñsdlaksñdlakñldaks dñlaskdñlasdk ñlasdkñlasdkñlasdkalñsdkñalsdk ñalsdk ñalsdk añlsdk ñalsd asld aksñldasñ ldka kdas ñldka ñsdañlaklañlask s sldk sld kqwodk asodkw kqwodk qwd'>
-        <Bento Bsv='2' Bsh='4' Bst='B1' Th='Proximas tareas:' Tp='1) Crear labels.' />
-        <Map Bsv='4' Bsh='2' Th='My location:' ARc='Y' theme={theme} />
-        <Bento Bsv='1' Bsh='2' Bst='B7' PBn='4' PBd='10' PBt='React:' PBimg={ReactImg} Com={ProgressBar}/>
-        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Theme switcher:' Com={() => <ThemeSwitch onChange={handleSwitch} />} />
-        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Button example:' Com={Button} />
-        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Download example:' Com={Download} />
-      </BentoGrid>
-      <BentoGrid Sh='HOLAAA' Sp='Chauuuuuñalsdañsdkañsldkañs ldkasñdaskdñ akñsdlaksñdlakñldaks dñlaskdñlasdk ñlasdkñlasdkñlasdkalñsdkñalsdk ñalsdk ñalsdk añlsdk ñalsd asld aksñldasñ ldka kdas ñldka ñsdañlaklañlask s sldk sld kqwodk asodkw kqwodk qwd'>
-        <Bento Bsv='2' Bsh='4' Bst='B1' Th='Proximas tareas:' Tp='1) Crear labels.' />
-        <Map Bsv='4' Bsh='2' Th='My location:' ARc='Y' theme={theme} />
-        <Bento Bsv='1' Bsh='2' Bst='B7' PBn='4' PBd='10' PBt='React:' PBimg={ReactImg} Com={ProgressBar}/>
-        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Theme switcher:' Com={() => <ThemeSwitch onChange={handleSwitch} />} />
-        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Button example:' Com={Button} />
-        <Bento Bsv='2' Bsh='2' Bst='B7' Th='Download example:' Com={Download} />
-      </BentoGrid>
       <Footer/>
     </>
   )
